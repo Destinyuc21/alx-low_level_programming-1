339281ee976eb39f67d71ff78cf5b8a99ac51bb8
Macmatthew <macahaotu@gmail.com>
2022-07-28 18:54:53 +0100

    more malloc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,44 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
 /**
- * isInteger - checks if s is an integer
- * @s: string to check
- * Return: 0 or 1
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return; Always 0 (Success)
  */
 
-int isInteger(const char *s)
+int check_num(char *str)
+
 {
-int i = 0;
-while (s[i] != '\0')
+/*Declaring variables*/
+unsigned int count;
+
+count = 0;
+while (count < strlen(str)) /*count string*/
+
 {
-	if (s[i] < '0' || s[i] > '9')
-		return (1);
-	i++;
-}
+if (!isdigit(str[count])) /*check if str there are digit*/
+{
 return (0);
 }
 
-/**
- * main - adds positive numbers
- * @argc: int
- * @argv: list
- * Return: 0
- */
-
-int main(int argc, char const *argv[])
-{
-int sum = 0;
-while (--argc)
-{
-	if (isInteger(argv[argc]))
-	{
-		printf("Error\n");
-		return (1);
-	}
-	sum += atoi(argv[argc]);
+count++;
+}
+return (1);
 }
 
-printf("%i\n", sum);
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+/*Declaring variables*/
+int count;
+int str_to_int;
+int sum = 0;
+
+count = 1;
+while (count < argc) /*Goes through the whole array*/
+{
+if(check_num(argv[count]))
+
+{
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
+}
+
+/*Condition if one of the number contains symbols that are not digits*/
+else
+{
+printf("Error\n");
+return (1);
+}
+
+count++;
+}
+
+printf("%d\n", sum); /*print sum*/
 
 return (0);
 }
